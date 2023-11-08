01c3a93ff309d1cae90b24e653492da6b0ffcb81
Sylwia <sylwia.szalacha1@gmail.com>
2023-11-08 22:45:45 +0100

    Tests to check if the range of numbers is correct

--- src/main/java/com/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/com/lotto/numberreceiver/NumberReceiverFacade.java
@@ -5,13 +5,11 @@ import java.util.Set;
 public class NumberReceiverFacade {
 
     public String receiveNumbers(Set<Integer> numbersFromUser) {
-        if (numbersFromUser.size() != 6) {
+        if ((numbersFromUser.size() != 6) || numbersFromUser.stream().anyMatch(number -> number < 1 || number > 99)) {
             return "fail";
         }
         return "success";
     }
-
-
-
-
 }
+
+
--- src/test/java/com/lotto/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/com/lotto/numberreceiver/NumberReceiverFacadeTest.java
@@ -4,49 +4,45 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NumberReceiverFacadeTest {
 
     @Test
-    public void shouldReturnFailWhenUserProvideLessThanSixNumbers (){
+    public void shouldReturnFailWhenUserProvideLessThanSixNumbers() {
         NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
         String result = numberReceiverFacade.receiveNumbers(Set.of(2, 4, 5, 7, 6));
-
-        assertEquals("fail",result);
-
-
+        assertEquals("fail", result);
     }
 
     @Test
-    public void shouldReturnSuccessWhenUserProvideSixNumbers (){
+    public void shouldReturnSuccessWhenUserProvideSixNumbers() {
         NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
-        String result = numberReceiverFacade.receiveNumbers(Set.of(2, 4, 5, 7, 6,12));
-
-        assertEquals("success",result);
-
-
-
+        String result = numberReceiverFacade.receiveNumbers(Set.of(2, 4, 5, 7, 6, 12));
+        assertEquals("success", result);
     }
+
     @Test
-    public void shouldReturnFailWhenUserProvideMoreThanSixNumbers (){
+    public void shouldReturnFailWhenUserProvideMoreThanSixNumbers() {
         NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
-        String result = numberReceiverFacade.receiveNumbers(Set.of(2, 4, 5, 7, 6,23,34));
-
-        assertEquals("fail",result);
-
-
+        String result = numberReceiverFacade.receiveNumbers(Set.of(2, 4, 5, 7, 6, 23, 34));
+        assertEquals("fail", result);
     }
 
     @Test
-    public void shouldReturnFailWhenUserProvideTheNumberOfOfRangeLangerThan99 (){
-
-
+    public void shouldReturnFailWhenUserProvideTheNumberOfOfRangeLargerThanNinetyNine() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        String result = numberReceiverFacade.receiveNumbers(Set.of(45, 34, 124, 125, 200, 100));
+        assertEquals("fail", result);
     }
 
     @Test
-    public void shouldReturnFailWhenUserProvideTheNumberOfOfRangeLowerThen1 (){
-
-
+    public void shouldReturnFailWhenUserProvideTheNumberOfOfRangeLowerThenOne() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        String result = numberReceiverFacade.receiveNumbers(Set.of(0, 4, 5, 7, 6, 100));
+        assertEquals("fail", result);
     }
+
+
+
 }
