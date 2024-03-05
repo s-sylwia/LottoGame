748fee730a58e6c02b094bb13862f1a9fc562db7
Sylwia <sylwia.szalacha1@gmail.com>
2024-03-05 22:56:23 +0100

    test added

--- src/test/java/com/lotto/domain/numbergenerator/NumberGeneratorFacadeTest.java
+++ src/test/java/com/lotto/domain/numbergenerator/NumberGeneratorFacadeTest.java
@@ -1,13 +1,47 @@
 package com.lotto.domain.numbergenerator;
 
+import com.lotto.domain.numbergenerator.dto.WinningTicketDto;
+import com.lotto.domain.numberreceiver.NumberReceiverFacade;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class NumberGeneratorFacadeTest {
 
+    private final WinningTicketRepository winningNumbersRepository = new WinningNumbersRepositoryTestImpl();
+    NumberReceiverFacade numberReceiverFacade = mock(NumberReceiverFacade.class);
 
     @Test
     public void shouldReturnWinningTicketByGivenDate() {
+        //given
+        LocalDateTime drawDate = LocalDateTime.of(2022, 12, 17, 12, 0, 0);
+        Set<Integer> generatedWinningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        String id = UUID.randomUUID().toString();
+        WinningTicket winningTicket = WinningTicket.builder()
+                .id(id)
+                .date(drawDate)
+                .winningNumbers(generatedWinningNumbers)
+                .build();
+        winningNumbersRepository.save(wi);
+        RandomTicketGenerable generator = new WinningNumberGeneratorTestImpl();
+        when(numberReceiverFacade.retrieveNextDrawDate()).thenReturn(drawDate);
+        WinningTicketGeneratorFacade numbersGenerator = new NumberGeneratorConfiguration().createForTest(generator, winningNumbersRepository, numberReceiverFacade);
+        //when
+        WinningTicketDto winningNumbersDto = numbersGenerator.retrieveWinningNumberByDate(drawDate);
+        //then
+        WinningTicketDto expectedWinningNumbersDto = WinningTicketDto.builder()
+                .date(drawDate)
+                .winningNumbers(generatedWinningNumbers)
+                .build();
+        assertThat(expectedWinningNumbersDto).isEqualTo(winningNumbersDto);
     }
+
 
 
     @Test
