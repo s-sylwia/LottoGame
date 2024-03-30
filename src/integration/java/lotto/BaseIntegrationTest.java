eca988b1eb5ea7277e45810b4aca401ec9d53205
Sylwia <sylwia.szalacha1@gmail.com>
2024-03-30 22:02:45 +0100

    modified BaseIntegrationTest

--- src/integration/java/lotto/BaseIntegrationTest.java
+++ src/integration/java/lotto/BaseIntegrationTest.java
@@ -1,13 +1,24 @@
 package lotto;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.lotto.AdjustableClock;
 import com.lotto.domain.LottoSpringBootApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.annotation.ApplicationScope;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @SpringBootTest(classes = LottoSpringBootApplication.class)
 @ActiveProfiles("integration")
@@ -15,9 +26,27 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class BaseIntegrationTest {
 
-    @@Autowired
-    public MockMvc mockMvc (){
+    @Autowired
+    public MockMvc mockMvc;
 
+    @Container
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @Autowired
+    public AdjustableClock clock;
+
+    @RegisterExtension
+    public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+//        registry.add("lotto.number-generator.http.client.config.port", () -> wireMockServer.getPort());
+//        registry.add("lotto.number-generator.http.client.config.uri", () -> WIRE_MOCK_HOST);
     }
-
 }
