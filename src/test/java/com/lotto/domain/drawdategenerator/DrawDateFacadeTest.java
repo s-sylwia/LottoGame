f1cbbeb9f291f6b3c2c561b36a9c59bd0d489c38
Sylwia <sylwia.szalacha1@gmail.com>
2024-04-15 23:25:31 +0200

    methods modified

--- src/test/java/com/lotto/domain/drawdategenerator/DrawDateFacadeTest.java
+++ src/test/java/com/lotto/domain/drawdategenerator/DrawDateFacadeTest.java
@@ -1,12 +1,10 @@
 package com.lotto.domain.drawdategenerator;
 
 import org.junit.jupiter.api.Test;
-
 import java.time.Clock;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
